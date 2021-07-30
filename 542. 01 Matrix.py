314fb98e910446f75788ee6e61a25e7aa8fa2951
AnnieBox <todearannie@gmail.com>
2021-07-29 22:03:39 -0700

    L33t

--- 542. 01 Matrix.py
+++ 542. 01 Matrix.py
@@ -3,31 +3,32 @@ class Solution:
         # Time = O(n), Space = O(n)
         rows = len(mat)
         cols = len(mat[0])
+        inf = 10**10
+        distance = [[inf] * cols for _ in range(rows)]
+        
         queue = collections.deque()
         
-        # float("inf")
-        inf = 10 ** 10
+        # def enqueue(r, c, d):
+        #     distance[r][c] = d
+        #     queue.append((r, c, d))
         
-        ans = [[inf] * cols for _ in range(rows)]
         
-        def enqueue(r, c, d):
-            ans[r][c] = d
-            queue.append((r, c, d))
-            
-            
         for r in range(rows):
             for c in range(cols):
                 if mat[r][c] == 0:
-                    enqueue(r, c, 0)
-             
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]    
+                    distance[r][c] = 0
+                    queue.append((r, c, 0))
+                    # enqueue(r, c, 0)
+        
+        directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
         while len(queue) > 0:
             r, c, d = queue.popleft()
-            
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
-                
-                if 0 <= nr < rows and 0 <= nc < cols and ans[nr][nc] == inf:
-                    enqueue(nr, nc, d+1)
-                    
-        return ans
+
+                if 0 <= nr < rows and 0 <= nc < cols and distance[nr][nc] == inf:
+                    queue.append((nr, nc, d + 1))
+                    distance[nr][nc] = d + 1
+                    # enqueue(nr, nc, d + 1)
+
+        return distance
