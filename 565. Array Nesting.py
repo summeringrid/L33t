1729afbd2e592fa22e2a42067d1832bd65fbbb58
AnnieBox <todearannie@gmail.com>
2021-09-01 17:34:18 -0700

    🙏

---
+++ 565. Array Nesting.py
@@ -0,0 +1,16 @@
+class Solution:
+    def arrayNesting(self, nums: List[int]) -> int:
+        N = len(nums)
+        visited = [False] * N
+        ans = 0
+
+        for num in nums:
+            length = 0
+
+            while not visited[num]:
+                visited[num] = True
+                length += 1
+                num = nums[num]
+
+            ans = max(length, ans)
+        return ans
