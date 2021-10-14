9db33030cf96234a8bd5c4f6482c50ec11654981
AnnieBox <todearannie@gmail.com>
2021-10-14 15:38:51 -0700

    L33t

--- Blind75/48. Rotate Image.py
+++ Blind75/48. Rotate Image.py
@@ -57,4 +57,57 @@ for row in D:
 
 
 
-# 38
+# 48 rotate image
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        # Approach I
+        # Time =O(M), M is the number of cells in the matrix
+        #         n = len(matrix)
+
+        #         for i in range(n//2 + n%2):
+        #             for j in range(n//2):
+        #                 temp = matrix[n-1-j][i]
+        #                 matrix[n-1-j][i] = matrix[n-1-i][n-j-1]
+        #                 matrix[n-1-i][n-j-1] = matrix[j][n-1-i]
+        #                 matrix[j][n-1-i] = matrix[i][j]
+        #                 matrix[i][j] = temp
+
+        # Approach II
+        # Time = O(n), n is the cell num of the matrix; Space = O(1)
+        #         self.transpose(matrix)
+        #         self.reflect(matrix)
+
+        #     def transpose(self, matrix):
+        #         n = len(matrix)
+
+        #         for i in range(n):
+        #             for j in range(i, n):
+        #                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        #     def reflect(self, matrix):
+        #         n = len(matrix)
+
+        #         for i in range(n):
+        #             for j in range(n//2):
+        #                 matrix[i][j], matrix[i][-j-1] = matrix[i][-j-1], matrix[i][j]
+
+        # Approach III
+        # Time = O(m) m is the cell num in the matrix (we can also say it is n^2, n is the len(matrix))
+        l, r = 0, len(matrix) - 1
+        while l < r:
+            for i in range(r - l):
+                top, bottom = l, r
+
+                topLeft = matrix[top][l + i]
+
+                matrix[top][l + i] = matrix[bottom - i][l]
+                matrix[bottom - i][l] = matrix[bottom][r - i]
+                matrix[bottom][r - i] = matrix[top + i][r]
+                matrix[top + i][r] = topLeft
+            l += 1
+            r -= 1
+
