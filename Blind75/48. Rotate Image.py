44df8ce86952e16be029cf80bc96b3987e6685f6
AnnieBox <todearannie@gmail.com>
2021-10-14 14:26:46 -0700

    L33t

--- Blind75/48. Rotate Image.py
+++ Blind75/48. Rotate Image.py
@@ -1,28 +1,35 @@
-# python syntax for creating a matrix
 import numpy as np
+
+print("python syntax for creating a matrix")
 
 A = [[1, 2, 3],
      [4, 5, 6],
      [7, 8, 9]]
 
+########################################
+print("approach I - nested for loop:")
+########################################
+# can't print out the matrix
+for row in range(len(A)):
+    for col in range(len(A[0])):
+        print(A[row][col], ' ', end='')
+    print('\n')
+########################################
+print("approach II - .join")
+########################################
+print([''.join(['{:1}'.format(item) for item in row])
+      for row in A])
 
-# # approach I
-# # can't print out the matrix
-# for i in range(len(A)):
-#     print("--")
-#     for j in range(len(A[0])):
-#         print(A[i][j])
+print('\n'.join(([''.join(['{:3}'.format(item) for item in row])
+      for row in A])))
 
 
-# # Approach II
-print('\n'.join([''.join(['{:3}'.format(item) for item in row])
-      for row in A]))
+########################################
+print("approach III - numpy")
+########################################
 
-# # Approach III
-print(np.array(A))
-
-# C = np.array([[1,2,3],[3,4,5],[7,8,9]])
-# print(C)
+C = np.array([[1,2,3],[3,4,5],[7,8,9]])
+print(C)
 
 
 B = [[None] * 3]*4
@@ -34,4 +41,20 @@ B = [[None] * 3]*4
 # print(B)
 
 print(np.array(B))
-print()
+print("try nested loop")
+
+D = [[1,2,3],[3,4,5],[7,8,9]]
+for row in range(len(D)):
+    for col in range(len(D[0])):
+        print(D[row][col], ' ', end="")
+    print('\n')
+print('-----------')
+for row in D:
+    for item in row:
+        print(item, ' ', end="")
+    print('\n')
+
+
+
+
+# 38
