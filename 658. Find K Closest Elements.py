172c222223137389e2afe223f51dc0e57cd77e7f
summeringrid <todearannie@gmail.com>
2022-02-20 23:32:36 -0800

    L33t

--- 658. Find K Closest Elements.py
+++ 658. Find K Closest Elements.py
@@ -24,9 +24,17 @@ def linear_solution(arr, x, k):
 # Approach II: binary solution ====================================
 
 def binary_solution(arr, x, k):
-    pass
+    l, r = 0, len(arr) - k  # sliding window of potential start point
+    while l < r:
+        m = (l + r) // 2
+        if x - arr[m] > arr[m + k] - x:  # go further to the right
+            l = m + 1
+        else:
+            r = m
+    return arr[l: l + k]
 
-# Time = O(logN + k) --> optimize (logN) -> log(N-k)
+    # Time = O(logN + k), binary search N elements in the arr, and construct k ele for the output
+    # Space = O(k)
 
 
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -34,4 +42,4 @@ x = 5
 k = 4
 
 print('linear search result:\n', linear_solution(nums, x, k), sep='')
-# print('binary search result:\n', binary_solution(nums, x, k), sep='')
+print('binary search result:\n', binary_solution(nums, x, k), sep='')
