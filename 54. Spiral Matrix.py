70f7e44d16e2b483c6503863d772f3b563fee8de
summeringrid <todearannie@gmail.com>
2022-04-15 23:07:16 -0700

    L33t

---
+++ 54. Spiral Matrix.py
@@ -0,0 +1,26 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+        l, r = 0, len(matrix[0])  # cols
+        top, bottom = 0, len(matrix)  # rows
+        # update the top & bottom, and l & r boundaries
+        while l < r and top < bottom:  # AND
+            for i in range(l, r):
+                res.append(matrix[top][i])
+            top += 1
+
+            for i in range(top, bottom):
+                res.append(matrix[i][r - 1])
+            r -= 1
+
+            if not (l < r and top < bottom):
+                break
+
+            for i in range(r - 1, l - 1, -1):
+                res.append(matrix[bottom - 1][i])
+            bottom -= 1
+
+            for i in range(bottom - 1, top - 1, -1):
+                res.append(matrix[i][l])
+            l += 1
+        return res
---
+++ test.py
@@ -0,0 +1,3 @@
+matrix = [[1,2,3],[4,5,6]]
+print(len(matrix))
+print(len(matrix[0]))
