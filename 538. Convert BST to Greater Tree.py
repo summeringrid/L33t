0f9707bae48f038fb962b411b9b3007ac26cddfd
summeringrid <todearannie@gmail.com>
2022-05-03 17:27:35 -0700

    L33t

---
+++ 538. Convert BST to Greater Tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        curSum = 0
+
+        def dfs(node):
+            if not node:
+                return
+            nonlocal curSum
+            dfs(node.right)
+            tmp = node.val
+            node.val += curSum
+            curSum += tmp
+            dfs(node.left)
+
+        dfs(root)
+        return root
+
+        # Time = O(n)
+        # Space = O(h), h = height of the tree
+        # if the tree is balanced, the height = log(n)
+    
