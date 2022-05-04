eadf94f9ec49e183327919f739d1e5b165e00531
summeringrid <todearannie@gmail.com>
2022-05-03 18:50:26 -0700

    L33t

--- 55. Jump Game.py
+++ 55. Jump Game.py
@@ -12,11 +12,11 @@ class Solution:
 
 
     # Approach II:
-    goal = len(nums) - 1
-    for i in range(len(nums) - 1, -1, -1):
-        if nums[i] + i >= goal:
-            goal = i
-    return not goal     # first num is not 0
+    # goal = len(nums) - 1
+    # for i in range(len(nums) - 1, -1, -1):
+    #     if nums[i] + i >= goal:
+    #         goal = i
+    # return not goal     # first num is not 0
 
 
 nums = [2,3,1,1,4]
