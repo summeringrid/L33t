5f2eec4af813a40e4250d9f25007892d590c2e3c
Ingrid Liu <todearannie@gmail.com>
2022-05-11 12:45:35 -0700

    Leet

--- Blind75/3. Longest Substring Without Repeating Characters.py
+++ Blind75/3. Longest Substring Without Repeating Characters.py
@@ -1,5 +1,21 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        # Approach I: Deque
+        check = deque()
+        res = 0
+
+        for c in s:
+            while c in check:
+                check.popleft()
+            else:
+                check.append(c)
+            res = max(res, len(check))
+        return res
+
+        # Time = O(n*res), n = len(s), res = maxLen of the unrepeated substr
+        # Space = O(m), m = len(check)
+
+        # Approach II: sliding window
         res = l = 0
         seen = set()
         for r in range(len(s)):
@@ -10,4 +26,5 @@ class Solution:
             res = max(res, r - l + 1)
         return res
 
-        # Time = O(n), Space = O(n)
+        # Time = O(n), n = len(s)
+        # Space = O(m), m = len(seen set)
